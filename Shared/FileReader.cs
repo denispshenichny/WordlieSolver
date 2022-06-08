ed43b59657ecffb190c6425895491797ce19b6b1
Denis Pshenichny <denispshenichny@yandex.ru>
2022-06-08 17:18:40 +0300

    words control

---
+++ Shared/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace WordlieSolver.Shared
+{
+    public static class FileReader
+    {
+        public static IEnumerable<string> ReadAllLines(string resourceName)
+        {
+            using Stream? stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"WordlieSolver.{resourceName}");
+            if (stream == null)
+                return Array.Empty<string>();
+
+            using StreamReader reader = new StreamReader(stream);
+            return reader.ReadToEnd().Split(new [] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
--- ViewModels/WordsViewModel.cs
+++ ViewModels/WordsViewModel.cs
@@ -1,6 +1,47 @@
-﻿namespace WordlieSolver.ViewModels
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Data;
+using Prism.Mvvm;
+using WordlieSolver.Shared;
+
+namespace WordlieSolver.ViewModels
 {
-    public class WordsViewModel
+    public class WordsViewModel : BindableBase
     {
+        private string _filterString = string.Empty;
+
+        public WordsViewModel()
+        {
+            IEnumerable<string> words = FileReader.ReadAllLines("Resources.russian_nouns.txt")
+                .Where(w => w.Length == 5);
+            AvailableWords = CollectionViewSource.GetDefaultView(words);
+            AvailableWords.Filter = WordsFilter;
+        }
+
+        private bool WordsFilter(object obj)
+        {
+            if (!(obj is string word))
+                return false;
+
+            return string.IsNullOrEmpty(word.Trim()) || word.Contains(FilterString);
+        }
+
+        public string FilterString
+        {
+            get => _filterString;
+            set
+            {
+                if (SetProperty(ref _filterString, value))
+                {
+                    AvailableWords.Refresh();
+                    RaisePropertyChanged(nameof(WordsCount));
+                }
+            }
+        }
+
+        public ICollectionView AvailableWords { get; }
+
+        public string WordsCount => $"Words fit: {AvailableWords.Cast<string>().Count()}";
     }
 }
